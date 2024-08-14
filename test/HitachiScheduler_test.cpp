338636fd231338bda4b84cff21055bd42664ee94
Marcelle Samir <marcelle.samir.s@gmail.com>
2024-08-14 20:28:55 +0000

    implement the unit tests to cover the latest changes

--- test/HitachiScheduler_test.cpp
+++ test/HitachiScheduler_test.cpp
@@ -3,167 +3,110 @@
 #include <chrono>
 #include <thread>
 
-class SchedulerTest : public ::testing::Test
-{
+void TaskAction() {
+    std::cout << "Task Executed" << std::endl;
+}
+
+class HitachiSchedulerTest : public ::testing::Test {
 protected:
-    void SetUp() override
-    {
-        // to be added
+    void SetUp() override {
     }
 
-    void TearDown() override
-    {
-        // to be added
+    void TearDown() override {
     }
 };
 
-TEST_F(SchedulerTest, FCFS_ScheduleTest)
-{
-    auto fcfsScheduler = std::make_unique<FCFS>();
-    HitachiScheduler scheduler(std::move(fcfsScheduler));
+TEST_F(HitachiSchedulerTest, SetAlgorithm) {
+    std::unique_ptr<SchedulingAlgorithm> roundRobinScheduler = std::make_unique<RoundRobin>();
+    HitachiScheduler scheduler(std::make_unique<FCFS>());
 
-    std::promise<void> task1Promise;
-    std::promise<void> task2Promise;
-    bool task1Executed = false;
-    bool task2Executed = false;
-
-    scheduler.addTask(std::make_unique<Task>([&]()
-    {
-        task1Executed = true;
-        task1Promise.set_value();
-    }, 1));
-
-    scheduler.addTask(std::make_unique<Task>([&]()
-    {
-        task2Executed = true;
-        task2Promise.set_value();
-    }, 2));
-
-    task1Promise.get_future().wait();
-    task2Promise.get_future().wait();
-
-    EXPECT_TRUE(task1Executed);
-    EXPECT_TRUE(task2Executed);
-}
-
-TEST_F(SchedulerTest, RoundRobin_ScheduleTest)
-{
-    auto roundRobinScheduler = std::make_unique<RoundRobin>();
-    HitachiScheduler scheduler(std::move(roundRobinScheduler));
-
-    std::promise<void> task1Promise;
-    std::promise<void> task2Promise;
-    bool task1Executed = false;
-    bool task2Executed = false;
-
-    scheduler.addTask(std::make_unique<Task>([&]()
-    {
-        task1Executed = true;
-        task1Promise.set_value();
-    }, 1));
-
-    scheduler.addTask(std::make_unique<Task>([&]()
-    {
-        task2Executed = true;
-        task2Promise.set_value();
-    }, 2));
-
-    task1Promise.get_future().wait();
-    task2Promise.get_future().wait();
-
-    EXPECT_TRUE(task1Executed);
-    EXPECT_TRUE(task2Executed);
-}
-
-TEST_F(SchedulerTest, SJN_ScheduleTest)
-{
-    auto sjnScheduler = std::make_unique<SJN>();
-    HitachiScheduler scheduler(std::move(sjnScheduler));
-
-    std::promise<void> task1Promise;
-    std::promise<void> task2Promise;
-    bool task1Executed = false;
-    bool task2Executed = false;
-
-    scheduler.addTask(std::make_unique<Task>([&]()
-    {
-        task1Executed = true;
-        task1Promise.set_value();
-    }, 1, 5));
-
-    scheduler.addTask(std::make_unique<Task>([&]()
-    {
-        task2Executed = true;
-        task2Promise.set_value();
-    }, 2, 3));
-
-    task1Promise.get_future().wait();
-    task2Promise.get_future().wait();
-
-    EXPECT_TRUE(task1Executed);
-    EXPECT_TRUE(task2Executed);
-}
-
-TEST_F(SchedulerTest, PriorityScheduling_ScheduleTest)
-{
-    auto priorityScheduler = std::make_unique<PriorityScheduling>();
-    HitachiScheduler scheduler(std::move(priorityScheduler));
-
-    std::promise<void> task1Promise;
-    std::promise<void> task2Promise;
-    bool task1Executed = false;
-    bool task2Executed = false;
-
-    scheduler.addTask(std::make_unique<Task>([&]()
-    {
-        task1Executed = true;
-        task1Promise.set_value();
-    }, 1));
-
-    scheduler.addTask(std::make_unique<Task>([&]()
-    {
-        task2Executed = true;
-        task2Promise.set_value();
-    }, 2));
-
-    task1Promise.get_future().wait();
-    task2Promise.get_future().wait();
-
-    EXPECT_TRUE(task1Executed);
-    EXPECT_TRUE(task2Executed);
-}
-
-TEST_F(SchedulerTest, HitachiScheduler_Test)
-{
-    auto fcfsScheduler = std::make_unique<FCFS>();
-    HitachiScheduler scheduler(std::move(fcfsScheduler));
-
-    std::promise<void> task1Promise;
-    bool task1Executed = false;
-
-    scheduler.addTask(std::make_unique<Task>([&]()
-    {
-        task1Executed = true;
-        task1Promise.set_value();
-    }, 1));
-
-    task1Promise.get_future().wait();
-    EXPECT_TRUE(task1Executed);
-
-    auto roundRobinScheduler = std::make_unique<RoundRobin>();
     scheduler.setAlgorithm(std::move(roundRobinScheduler));
 
-    std::promise<void> task2Promise;
-    bool task2Executed = false;
+    EXPECT_NO_THROW(scheduler.startScheduler());
+}
 
-    scheduler.addTask(std::make_unique<Task>([&]()
-    {
-        task2Executed = true;
-        task2Promise.set_value();
-    }, 2));
+TEST_F(HitachiSchedulerTest, AddTaskFCFS) {
+    std::unique_ptr<SchedulingAlgorithm> fcfsScheduler = std::make_unique<FCFS>();
+    HitachiScheduler scheduler(std::move(fcfsScheduler));
 
-    task2Promise.get_future().wait();
-    EXPECT_TRUE(task2Executed);
+    auto task = std::make_unique<Task>(TaskAction, 1, 5);
+    scheduler.addTask(std::move(task));
+
+    EXPECT_NO_THROW(scheduler.startScheduler());
+}
+
+TEST_F(HitachiSchedulerTest, AddTaskRoundRobin) {
+    std::unique_ptr<SchedulingAlgorithm> roundRobinScheduler = std::make_unique<RoundRobin>();
+    HitachiScheduler scheduler(std::move(roundRobinScheduler));
+
+    auto task = std::make_unique<Task>(TaskAction, 1, 5);
+    scheduler.addTask(std::move(task));
+
+    EXPECT_NO_THROW(scheduler.startScheduler());
+}
+
+TEST_F(HitachiSchedulerTest, AddTaskSJN) {
+    std::unique_ptr<SchedulingAlgorithm> sjnScheduler = std::make_unique<SJN>();
+    HitachiScheduler scheduler(std::move(sjnScheduler));
+
+    auto task = std::make_unique<Task>(TaskAction, 1, 5);
+    scheduler.addTask(std::move(task));
+
+    EXPECT_NO_THROW(scheduler.startScheduler());
+}
+
+TEST_F(HitachiSchedulerTest, AddTaskPriorityScheduling) {
+    std::unique_ptr<SchedulingAlgorithm> priorityScheduler = std::make_unique<PriorityScheduling>();
+    HitachiScheduler scheduler(std::move(priorityScheduler));
+
+    auto task = std::make_unique<Task>(TaskAction, 1, 5);
+    scheduler.addTask(std::move(task));
+
+    EXPECT_NO_THROW(scheduler.startScheduler());
+}
+
+TEST_F(HitachiSchedulerTest, ExecuteTasksFCFS) {
+    std::unique_ptr<SchedulingAlgorithm> fcfsScheduler = std::make_unique<FCFS>();
+    HitachiScheduler scheduler(std::move(fcfsScheduler));
+
+    for (int i = 0; i < 3; ++i) {
+        scheduler.addTask(std::make_unique<Task>([i]() { std::cout << "Task " << i << " Executed" << std::endl; }, i, 5));
+    }
+
+    EXPECT_NO_THROW(scheduler.startScheduler());
+}
+
+TEST_F(HitachiSchedulerTest, ExecuteTasksRoundRobin) {
+    std::unique_ptr<SchedulingAlgorithm> roundRobinScheduler = std::make_unique<RoundRobin>();
+    HitachiScheduler scheduler(std::move(roundRobinScheduler));
+
+    for (int i = 0; i < 3; ++i) {
+        scheduler.addTask(std::make_unique<Task>([i]() { std::cout << "Task " << i << " Executed" << std::endl; }, i, 5));
+    }
+
+    EXPECT_NO_THROW(scheduler.startScheduler());
+}
+
+TEST_F(HitachiSchedulerTest, ExecuteTasksSJN) {
+    std::unique_ptr<SchedulingAlgorithm> sjnScheduler = std::make_unique<SJN>();
+    HitachiScheduler scheduler(std::move(sjnScheduler));
+
+    for (int i = 0; i < 3; ++i) {
+        scheduler.addTask(std::make_unique<Task>([i]() { std::cout << "Task " << i << " Executed" << std::endl; }, i, 5));
+    }
+
+    EXPECT_NO_THROW(scheduler.startScheduler());
+}
+
+TEST_F(HitachiSchedulerTest, ExecuteTasksPriorityScheduling) {
+    std::unique_ptr<SchedulingAlgorithm> priorityScheduler = std::make_unique<PriorityScheduling>();
+    HitachiScheduler scheduler(std::move(priorityScheduler));
+
+    for (int i = 0; i < 3; ++i) {
+        scheduler.addTask(std::make_unique<Task>([i]() { std::cout << "Task " << i << " Executed" << std::endl; }, i, 5));
+    }
+
+    EXPECT_NO_THROW(scheduler.startScheduler());
 }
 
 int main(int argc, char **argv)
